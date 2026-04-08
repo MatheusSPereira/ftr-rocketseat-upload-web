fb738017823650b3aed0643af0bce2ca525b86f8
MatheusSPereira <githubmatheus@outlook.com>
2026-04-08 16:34:44 -0300

    Estrutura do Widget

---
+++ upload-web/src/components/upload-widget.tsx
@@ -0,0 +1,17 @@
+import { UploadWidgetDropzone } from "./upload-widget-dropzone";
+import { UploadWidgetHeader } from "./upload-widget-header";
+import { UploadWidgetUploadList } from "./upload-widget-upload-list";
+
+export function UploadWidget() {
+    return (
+        <div className="bg-zinc-900 w-full overflow-hidden max-w-90 rounded-xl shadow-shape">
+            <UploadWidgetHeader />
+
+            <div className="flex flex-col gap-4 py-4">
+                <UploadWidgetDropzone />
+                <div className="h-px bg-zinc-800 border-t border-black/50 box-content" />
+                <UploadWidgetUploadList />
+            </div>
+        </div>
+    )
+}
